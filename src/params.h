b1268d080b999141c20069dd0086834234183056
Asad <asad.norouzi@gmail.com>
2020-01-01 21:26:02 -0500

    Add the global parameters header file

---
+++ src/params.h
@@ -0,0 +1,12 @@
+#ifndef PARAMS_H
+#define PARAMS_H
+
+namespace params
+{
+  const int GAP_BUF = 25;
+  const int WAYPOINTS_GAP = 30;
+
+  const double MAX_VEL = 49.80;
+  const double MAX_ACC = 0.224;
+}
+#endif
